5af1c4b6809a9545ea3be4aa454e19b48e4a06a4
lailanoviasari <laila.20013@mhs.unesa.ac.id>
2023-11-03 19:40:30 +0700

    add file tugas sesi 33 (4 nomor)

---
+++ sesi33.php
@@ -0,0 +1,51 @@
+<?php
+
+# NOMOR 1
+echo 'Nomor 1 <br><br>';
+
+# deklarasi array untuk nomor 1
+$array_no1 = array('satu', 'dua', 'tiga', 'empat', 'lima');
+
+# perulangan untuk memanggil data array dari index yang terakhir
+for ($i=count($array_no1)-1; $i >=0 ; $i--) { 
+    echo $array_no1[$i] . '<br>';
+}
+
+echo '<br><hr><br>';
+
+# NOMOR 2
+echo 'Nomor 2 <br><br>';
+
+# deklarasi array untuk nomor 2
+$array_no2 = array('apel', 'nanas', 'mangga', 'jeruk');
+
+$jumlah_array = count($array_no2);
+
+echo 'Terdapat ' . $jumlah_array . ' buah';
+
+echo '<br><br><hr><br>';
+
+# NOMOR 3
+echo 'Nomor 3 <br><br>';
+
+# deklarasi array untuk nomor 3
+$array_no3 = array(7, 3, 4, 9);
+
+$total = 0;
+
+foreach ($array_no3 as $key => $value) {
+    $total += $value;
+}
+
+echo 'Totalnya adalah ' . $total;
+
+echo '<br><br><hr><br>';
+
+# NOMOR 4
+echo 'Nomor 4 <br><br>';
+
+for ($i=1; $i <= 10; $i++) { 
+    echo '1x'.$i.'='.(1*$i).'<br>';
+}
+
+?>
